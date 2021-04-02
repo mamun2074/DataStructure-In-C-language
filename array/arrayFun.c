93507d2cb64800062b9043e798cb573612d78cf4
mamun2074 <mamun120520@gmail.com>
2021-04-02 22:40:18 +0600

    added min max function

--- array/arrayFun.c
+++ array/arrayFun.c
@@ -3,7 +3,7 @@
 // Array structer
 struct array
 {
-    int arr[15];
+    int arr[12];
     int size;
     int length;
 };
@@ -26,13 +26,43 @@ void Set(int arr[], int length, int index, int x)
     }
 }
 
+// Find Max value
+int Max(struct array arr)
+{
+    int max, i;
+    max = arr.arr[0];
+    for (i = 1; i < arr.length; i++)
+    {
+        if (arr.arr[i] > max)
+        {
+            max = arr.arr[i];
+        }
+    }
+    return max;
+}
+
+// Find minimum value on array
+int Min(struct array arr)
+{
+    int min, i;
+    min = arr.arr[0];
+    for (i = 1; i < arr.length; i++)
+    {
+        if (min > arr.arr[i])
+        {
+            min = arr.arr[i];
+        }
+    }
+    return min;
+}
+
 int main()
 {
-    struct array arr = {{5, 8, 10, 12, 15, 20, 25, 27, 28, 35, 40}, 15, 11};
+    struct array arr = {{5, 8, 10, 12, 15, 20, 25, 27, 28, 35, 40}, 12, 11};
 
     Set(arr.arr, arr.length, 1, 200);
 
-    printf("Value is : %d", Get(arr,1));
+    printf("Value is : %d", Min(arr));
 
     return 0;
 }
