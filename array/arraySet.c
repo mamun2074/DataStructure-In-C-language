5f0f8e38947dd5e61b08f0af3676408fb875f5ee
mamun2074 <mamun120520@gmail.com>
2021-04-09 09:45:56 +0600

    added intersection on sorted arrays and unsorted arrays

--- array/arraySet.c
+++ array/arraySet.c
@@ -86,15 +86,69 @@ void unionSortedArray(struct array arA, struct array arB, struct array *arC)
     }
 }
 
+// intersection unsorted arrays O(n2)
+void intersectionUnsortedArray(struct array arA, struct array arB, struct array *arC)
+{
+    int i, j, k, flag;
+    k = 0;
+    for (i = 0; i < arA.length; i++)
+    {
+        flag = 0;
+        for (j = 0; j < arB.length; j++)
+        {
+            if (arA.arr[i] == arB.arr[j])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            arC->arr[k++] = arA.arr[i];
+            arC->length++;
+        }
+    }
+}
+
+// intersection sorted arrays O(n)
+void intersectionSortedArray(struct array arA, struct array arB, struct array *arC)
+{
+    int i, j, k;
+    i = j = k = 0;
+    while (i < arA.length && j < arB.length)
+    {
+        /* code */
+        if (arA.arr[i] == arB.arr[j])
+        {
+            arC->arr[k++] = arA.arr[i++];
+            j++;
+            arC->length++;
+        }
+        else if (arA.arr[i] < arB.arr[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+
 int main()
 {
+    // struct array arrA = {{3, 5, 10, 4, 6}, 5, 8};
+    // struct array arrB = {{12, 4, 7, 2, 5}, 5, 8};
+
     struct array arrA = {{3, 4, 5, 6, 10}, 5, 8};
     struct array arrB = {{2, 4, 5, 7, 12}, 5, 8};
+
     struct array arrC;
     arrC.size = 10;
+    arrC.length = 0;
     display(arrA);
     display(arrB);
-    unionSortedArray(arrA, arrB, &arrC);
+    intersectionSortedArray(arrA, arrB, &arrC);
     display(arrC);
     return 0;
 }
